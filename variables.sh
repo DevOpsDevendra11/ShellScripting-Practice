26c690100ec1577d0ff408b7efb44f70064dc6a8
Devendra <devopsdevendra11@gmail.com>
2026-03-30 19:03:25 +0530

    First Commit from variables file

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "PERSON1 : Hello PERSON2 Nice to meet you"
-echo "PERSON2 : Hello PERSON1 same from myside"
-echo "PERSON1 : Can we have a deal"
-echo "PERSON2 : If the Ukraine joins with Russia its Ok"
+echo "$PERSON1 : Hello $PERSON2 Nice to meet you"
+echo "$PERSON2 : Hello $PERSON1 same from myside"
+echo "$PERSON1 : Can we have a deal"
+echo "$PERSON2 : If the Ukraine joins with Russia its Ok"
