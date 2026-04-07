d64a01a3c0801ac10031134964c71dd9cf46c387
Devendra <devopsdevendra11@gmail.com>
2026-04-07 15:43:03 +0530

    added the usage of the read command

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo " Please enter your PIN Number"
-read PIN_Number  # The PIN_Number is the variable
+read -s PIN_Number  # The PIN_Number is the variable  -s is used to hide the entered Pin in the terminal.
 echo "The entered pin is : $PIN_Number"
